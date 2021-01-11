9ee0b5b28f64867635de69a5fc4228d4fd8340ac
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-01-10 19:33:22 -0800

    Finished as much JS as I could

--- index.js
+++ index.js
@@ -209,11 +209,16 @@ Practice accessing data above by console.log-ing following items:
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
+console.log(artists[0].name);
+console.log(artists[2].bio);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
+// console.log(artists[8].name = "Vincent van Gough");
+artists[8].name = "Vincent van Gough";
+console.log(artists[8].name);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -225,18 +230,40 @@ Create a function called `getArtistByIndex` that takes two arguments:
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-function getArtistByIndex(/*Your Code Here*/) {
+// function getArtistByIndex(index, artistArray) {
+//   /*Your Code Here*/
+//   return 'The artist at index ${index["index"]} is ${artistArray["name"]}.'
+// }
+// console.log(getArtistByIndex(artists["id"], artists["name"]));
+
+function getArtistByIndex(artistArray, index) {
   /*Your Code Here*/
+  return `the artist at index ${artistArray[index]["id"]} is ${artistArray[index]["name"]}`
 }
+console.log(getArtistByIndex(artists, 0));
+
 
   
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
-Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
+Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]
 
-function get20s(/*Your Code Here*/){
+1. create a new array
+2. check thru each artist for loop, need split */
+
+function get20s(array){
   /*Your Code Here*/
+const newArr = [];
+for(let i=0; i<array.length; i++){
+  const str = array[i]["years"].split("-");
+ if(str[0] >=1900 && str[1] <= 2000){
+   newArr.push(array[i]["name"])
+ }
 }
+return newArr;
+}
+
+console.log(get20s(artists))
 
 
 
@@ -251,10 +278,12 @@ Create a function called `removeArtist` that takes two arguments:
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+
+function removeArtist(artistArray, index) {
+  artistArray.splice(index,1);      
+  return artistArray.length;
 }
-   
+console.log(removeArtist(artists, 0)); 
 
 /**
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -270,9 +299,22 @@ bio: Add 1-2 sentences (or use lorem ipsum)
 
 At the end, this function should return the new array with information added"*/
 
-function addArtist(/*Your Code Here*/){
-  /*Your Code Here*/
+function addArtist(artist){
+  artist.push({id:20, name:"Ali", years:"1985 - 2020", genre: "Web Design", nationality: "African-American", bio:"Alieze was born in Pittsburgh, PA. She went to Penn State and is now studying Web Design at Lambda."});
+  
+  return artist;
   }
+  console.log(addArtist(artists));
+    
+
+// function addArtist(artist){
+//   let newArr = artists.push(artist);
+//   // console.log(newArr);
+//   return newArr;
+//   }
+//     console.log(addArtist({id:20, name:"Ali", years:"1985 - 2020", genre: "Web Design", nationality: "African-American", bio:"Alieze was born in Pittsburgh, PA. She went to Penn State and is now studying Web Design at Lambda."}));
+
+
 
   
 
@@ -285,8 +327,11 @@ and returns an array with names of artists who painted more than 100 paintings.
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ..."Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/){
+function lotsOfArt(artistsArray){
   /*Your Code Here*/
+  // for (let i=0; i>100; i++)
+  //    if (S)
+
 }
 
 
